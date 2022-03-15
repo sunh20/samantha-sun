c6d7d705f55995ecd3d808d9821430ac7109711f
Samantha <sunh20@uw.edu>
2022-03-14 18:26:19 -0700

    Revert "bash script to auto update site"
    This reverts commit 775320cbf00777670167237c0b3434e4aa33a01d.

--- update_site.sh
+++
@@ -1,1 +0,0 @@
-bundle exec jekyll build; git checkout site; mv _site ../; rm -rf ./*; cp -r ../_site/* ./; git add -u; git status; git commit -m 'site update'; git pull; git push; git checkout master; rm -rf ../_site/; echo update complete;
