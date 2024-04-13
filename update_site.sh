fd0f3267448c2b17e230f6789c68e7f8d1376008
Samantha <sunh20@uw.edu>
2024-04-13 13:47:56 -0700

    for adding new files to site

--- update_site.sh
+++ update_site.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build; git checkout site; mv _site ../; rm -rf ./*; cp -r ../_site/* ./; git add -u; git status; git commit -m 'site update'; git pull; git push; git checkout master; rm -rf ../_site/; echo update complete;
+bundle exec jekyll build; git checkout site; mv _site ../; rm -rf ./*; cp -r ../_site/* ./; git add *; git status; git commit -m 'site update'; git pull; git push; git checkout master; rm -rf ../_site/; echo update complete;
